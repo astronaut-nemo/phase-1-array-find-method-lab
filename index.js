5d4b49961852186ffe14286f0997f7b44f67d533
Joanna <astronautnemo@gmail.com>
2022-11-26 18:14:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+
+function checkYear(element){
+  return (element.result === 'W')
+}
+
+function superbowlWin(record){
+  if(record.find(checkYear) != undefined){
+    return record.find(checkYear).year
+  } else {return record.find(checkYear)}
+}
